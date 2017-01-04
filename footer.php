e17acc8eb35cc259c52b1183b6640c714d7d2a13
Alberto Garrido Pacheco <albertogp1991@gmail.com>
2017-01-04 13:50:05 +0100

    añadido estilo al footer, añadido footer a registro.php, añadido fonts-awesome.css y carpeta fonts

--- footer.php
+++ footer.php
@@ -1,15 +1,22 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <br>
-        <div id="footer">
-            <a href="quienesSomos.php">Quienes Somos</a>
-            <a href="queHacemos.php">Que Hacemos</a>
-            <a href="dondeEstamos.php">Donde Estamos</a>
-            <a href="politicas.php">Politicas</a>
-        </div>
-    </body>
+<!-- Footer -->
+<div id="footer">
+    <ul class="icons">
+        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+        <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+        <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+    </ul>
+    <ul class="enlacesFooter">
+        <li>&copy; PA-Proyecto</li>
+        <li><a href="quienesSomos.php">Quienes Somos</a></li>
+        <li><a href="queHacemos.php">Que Hacemos</a></li>
+        <li><a href="dondeEstamos.php">Donde Estamos</a></li>
+        <li><a href="politicas.php">Politicas</a></li>
+    </ul>
+</div>
+
+<!-- Scripts -->
+
+
+</body>
 </html>
