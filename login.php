a9aa6b93aa4b1edc872e0fceba25e993780befed
rmonrec <ricardo_montero_recuero@hotmail.com>
2017-01-04 13:15:31 +0100

    Añadido CRUDPeticion_amistad, editado login para usar el CRUDUsuario, Añadida Carpeta CRUD y movidas todas las CRUDs #19

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 //Se inicia session
 session_start();
+include_once './CRUD/CRUDUsuario.php';
 //Si se ha pulsado sobre el botón de login
 if (isset($_POST['btnLogin'])) {
     if ($_POST['correo'] != "" && $_POST["password"] != "") {
@@ -12,43 +13,11 @@ if (isset($_POST['btnLogin'])) {
         //Para evitar las inyeciones SQL
         $correo = filter_var($correo, FILTER_SANITIZE_MAGIC_QUOTES);
         $password = filter_var($password, FILTER_SANITIZE_MAGIC_QUOTES);
-
-        //Conexión a base de datos
-        $con = mysqli_connect("localhost", "root", "");
-
-        if (!$con) {
-            die("Error al conectarse a la base de datos");
-        }
-
-        //Seleccion de base de datos
-        $db = mysqli_select_db($con, 'social_football');
-        if (!$db) {
-            die("Error al seleccionar la base de datos");
-        }
-
-        //Consulta para comprobar si existe o no el usuario
-        $result = mysqli_query($con, 'SELECT * FROM usuario WHERE correo_usuario LIKE "' . $correo . '" AND pass_usuario LIKE "' . $password . '"');
-
-        //Si hay un solo resultado significa que es correcto
-        if (mysqli_num_rows($result) == 1) {
-            //Se añade al array global Session valores
-            $_SESSION['errorLogin'] = False;
-            $col = mysqli_fetch_array($result);
-            $_SESSION['idUserLogin'] = $col['id_usuario'];
-            $_SESSION['nombreUserLogin'] = $col['nombre_usuario'];
-            $_SESSION['apellido1UserLogin'] = $col['apellido1_usuario'];
-            $_SESSION['apellido2UserLogin'] = $col['apellido2_usuario'];
-            $_SESSION['equipoUser'] = $col['equipo_id'];
-            $_SESSION['admin'] = $col['admin'];
-
+        
+        if (readUsuario($correo, $password)){
             header('location: inicio.php');
-        } else {
-            $_SESSION['errorLogin'] = True;
         }
-        mysqli_close($con);
-    } /*else {
-        echo '<span style="color:red"><h3>Debe rellenar los campos</h3></span>';
-    }*/
+    }
 }
 //Si se pulsa sobre el botón de registrarse, se manda al formulario
 if (isset($_POST['btnRegistro'])) {
@@ -65,10 +34,10 @@ if (isset($_POST['btnRegistro'])) {
     </head>
     <body>
         <?php
-        if (isset($_SESSION['errorLogin']) && $_SESSION['errorLogin'] == True) {
+        /*if (isset($_SESSION['errorLogin']) && $_SESSION['errorLogin'] == True) {
             echo '<span style="color:red"><h3>ERROR, EL USUARIO NO EXISTE</h3></span>';
             session_destroy();
-        }
+        }*/
         ?>
         <section id="fondo">
             <h2>Social Football</h2>
