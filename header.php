ca876bdb1985afabde670ebedf1cc5501c5f334e
Alberto Garrido Pacheco <albertogp1991@gmail.com>
2017-01-04 15:51:01 +0100

    preparando header.php, corrigiendo registro.php y algún detalle de estilos.css

--- header.php
+++ header.php
@@ -1,25 +1,47 @@
 
 <html>
     <head>
+        <title>Social Football</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="css/estilos.css" />
+
     </head>
     <body>
-        <div id="header">
+        <header id="header">
+            <h1><a href="index.html">PA-Equipo-07</a></h1>
+            <nav id="nav">
+                <ul>
+                    <li class="special">
+                      <!--  <a href="#menu" class="menuToggle"><span>Menu</span></a>
+                        <div id="menu">
+                            <ul>-->
+                    <li><a href="inicio.php">Inicio</a></li>
+                    <li><a href="partidos.php">Partidos</a></li>
+                    <li><a href="mensajes.php">Mensajes</a></li>
+                    <li><a href="amigos.php">Amigos</a></li><?php
+                    if ($_SESSION['admin'] == 0) {
+                        ?>
+                        <li><a href="noticias.php">Noticias</a></li>
+                        <li><a href="equipos.php">Equipos</a></li>
+                        <?php
+                    }
+                    ?>
+                    <li><a href="logout.php">Cerrar Sesión</a></li>
+                    <!-- </ul>
+                 </div>
+             </li>-->
+                </ul>
+            </nav>
+        </header>
+
+       <!-- <div id="header">
             <a href="inicio.php">Inicio</a>
             <a href="partidos.php">Partidos</a>
             <a href="mensajes.php">Mensajes</a>
             <a href="amigos.php">Amigos</a>
-            
-            <?php
-                if ($_SESSION['admin']==0){
-                    ?>
-                    <a href="noticias.php">Noticias</a>
-                    <a href="equipos.php">Equipos</a>
-                    <?php
-                }
-            ?>
-           <a href="logout.php">Cerrar Sesión</a>
-        </div>
-    </body>
-</html>
+
+           
+            <a href="logout.php">Cerrar Sesión</a>
+        </div> -->
+
