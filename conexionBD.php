5f5ffe8019e7ffb4042f8a4afb34ef1785056f13
Alberto Garrido Pacheco <albertogp1991@gmail.com>
2017-01-11 19:00:08 +0100

    arreglando acentos en conexionDB

--- conexionBD.php
+++ conexionBD.php
@@ -1,13 +1,15 @@
 <?php
+
 //Realiza la conexion a la base de datos y devuelve el conector
 function conectaBD() {
     $con = mysqli_connect("localhost", "root", "");
+    mysqli_set_charset($con, "utf8"); /* arregla acentos */
     if (!$con) {
         die("Error al conectarse a la base de datos");
     }
-    $db = mysqli_select_db($con,'social_football');   
+    $db = mysqli_select_db($con, 'social_football');
     if (!$db) {
-        die("Error al seleccionar la base de datos". mysqli_error($con));
+        die("Error al seleccionar la base de datos" . mysqli_error($con));
     }
     return $con;
 }
