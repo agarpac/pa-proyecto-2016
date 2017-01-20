4167fbf50ad1ecc8d04658714d7473c01d698aa4
Alberto Garrido Pacheco <albertogp1991@gmail.com>
2017-01-21 00:35:25 +0100

    Detalles corregido

--- modificarEquipo.php
+++ modificarEquipo.php
@@ -23,7 +23,7 @@ if (isset($_SESSION['usuario_logueado']) && $_SESSION['usuario_logueado']) {
             <form action="#" method="POST">
                 <label><span>Escudo:</span></label><img src="<?php echo $_SESSION['foto_equipo_modificar']; ?>" alt="equipo<?php echo $_SESSION['id_equipo_modificar']; ?>" width="40px"/>
                 <label><span>Nombre:<span class="required"> * </span></span><input type="text" class="input-field" name="nombre" value="<?php echo $_SESSION['nombre_equipo_modificar']; ?>"/></label>
-                <label><span>Año de fundación:<span class="required"> * </span></span><input type="number" class="input-field" name="anio" value="<?php echo $_SESSION['anio_fundacion_modificar']; ?>" /></label>
+                <label><span>Fundación:<span class="required">*</span></span><input type="number" class="input-field" name="anio" value="<?php echo $_SESSION['anio_fundacion_modificar']; ?>" /></label>
 
                 <input type="submit" class="buttonSpecial" value="Aceptar" name="btnAceptar" />
                 <input type="submit" class="buttonSpecial" value="Cancelar" name="btnCancelar" />
